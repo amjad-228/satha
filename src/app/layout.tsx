d89620ece6aa1a2d3a6b3e3d311bf26da150c4ed
amjad <amjadalsabry28@gmail.com>
2024-12-09 06:19:13 +0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({
 
         {/* Canonical and Alternate Links */}
         <link rel="canonical" href="https://sathaapp.com" />
-
+        <link rel="alternate" href="https://sathaapp.com/#services" hrefLang="ar-sa" />
         <link rel="alternate" href="https://sathaapp.com" hrefLang="x-default" />
         
         
