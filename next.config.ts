637f2a4d79a938081958f82aa5f96c7769cc5e6c
amjad <amjadalsabry28@gmail.com>
2024-12-10 06:00:21 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'https://sathaapp.com/:path*',
-        permanent: true, // إعادة توجيه 301 دائمة
-      },
-    ];
-  },
 };
 
 export default nextConfig;
+
+
