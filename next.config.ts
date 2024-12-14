0580cd93bb534b761a06e04b3fd8c25d9c183c3d
amjad <amjadalsabry28@gmail.com>
2024-12-14 11:20:33 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -9,15 +9,15 @@ const nextConfig: NextConfig = {
         has: [
           {
             type: 'host', // نحدد أن التوجيه يحدث بناءً على المضيف
-            value: 'www.sathaapp.com', // إذا كان الرابط يحتوي على www
+            value: 'sathaapp.com', // إذا كان الرابط يحتوي على www
           },
         ],
-        destination: 'https://sathaapp.com/:path*', // التوجيه إلى sathaapp.com بدون www
+        destination: 'https://www.sathaapp.com/:path*', // التوجيه إلى sathaapp.com بدون www
         permanent: true, // التوجيه دائم (301)
       },
     ]
   },
-  
+
   // تفعيل وضع Strict Mode ل React
   reactStrictMode: true, 
   
