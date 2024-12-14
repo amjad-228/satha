3f98020025180f3953bfdf8247ff74a3f21da4a5
amjad <amjadalsabry28@gmail.com>
2024-12-14 05:34:33 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,48 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  // تفعيل وضع Strict Mode ل React
+  reactStrictMode: true, 
+  
+  // تفعيل تقليص JavaScript
+  swcMinify: true, 
 
-export default nextConfig;
+  // إعدادات Webpack لتقليل حجم الملفات
+  webpack(config) {
+    config.optimization = {
+      ...config.optimization,
+      splitChunks: {
+        chunks: 'all',
+      },
+    }
+    return config
+  },
 
+  // إعدادات الصور لتحسينها
+  images: {
+    domains: ['sathaapp.com'], // إضافة المجالات التي تحتوي على الصور في موقعك
+    formats: ['image/avif', 'image/webp'], // إضافة تنسيقات صور جديدة لتقليل الحجم
+  },
 
+  // تكوين إعدادات الأمان
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Content-Type-Options', value: 'nosniff' }, // منع المتصفح من محاولة اكتشاف نوع الملف
+          { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains' }, // تعزيز أمان الاتصال عبر HTTPS
+        ],
+      },
+    ]
+  },
+
+  // تمكين استعمال PWA
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
+}
+
+export default nextConfig
