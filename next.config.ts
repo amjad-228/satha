4aa31184c5a835b0d5401d5817a7673780664b97
amjad <amjadalsabry28@gmail.com>
2024-12-14 06:50:44 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -31,6 +31,7 @@ const nextConfig: NextConfig = {
       {
         source: '/(.*)',
         headers: [
+          { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
           { key: 'X-Content-Type-Options', value: 'nosniff' }, // منع المتصفح من محاولة اكتشاف نوع الملف
           { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains' }, // تعزيز أمان الاتصال عبر HTTPS
         ],
