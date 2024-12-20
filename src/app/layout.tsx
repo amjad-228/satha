fc49b376a4c329aebd10742fc3716bf41339f26c
amjad <amjadalsabry28@gmail.com>
2024-12-20 03:11:45 +0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -61,6 +62,17 @@ export default function RootLayout({
         <meta name="description" content={"سطحة جدة هي اقرب سطحة في جدة توفر خدمة نقل وسحب السيارات وتقديرها في شيخ المعارض. سطحة جدة ايضا تقدم خدمة فتح أبواب السيارات المقفلة بكل احترافية وامان."} />
         <meta name="keywords" content={"سطحة جدة, تطبيق سطحة, اقرب سطحة, سطحة لنقل السيارات, خدمة فتح الأبواب, السيارات المتعطلة, جدة, سطحة قريبة مني, خدمة سحب السيارات في جدة, سطحة لنقل السيارات, سيارات الطوارئ جدة"} />
         <link rel="icon" href="/favicon.ico" />
+
+        {/* Google Analytics */} 
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Y7HMBRLRML" 
+        />
+        <Script id="google-analytics">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-Y7HMBRLRML', { 'cookie_flags': 'SameSite=None; Secure' });
+          `}
+        </Script>
         
         {/* Primary Meta Tags */}
         <meta charSet="UTF-8" />
