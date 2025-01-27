d9973b0a1ada34ec3daf1502ebc0e71a8f1c1307
amjad-228 <amjadalsabry28@gmail.com>
2025-01-27 19:41:45 +0300

    first commit

--- src/app/satahat-jeddah/Sathat-Jeddah-Alhamraa/page.tsx
+++ src/app/satahat-jeddah/Sathat-Jeddah-Alhamraa/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import { Phone, Clock, CreditCard, MapPin } from "lucide-react"
 import type { Metadata } from "next"
+import { FaPhone, FaWhatsapp } from "react-icons/fa"
 
 
 export const metadata: Metadata = {
@@ -62,13 +63,30 @@ export default function SathatJeddahAlhamraaPage() {
       <div className="bg-blue-100 p-4 rounded-lg">
         <h3 className="text-xl font-semibold mb-2">اطلب سطحة الآن</h3>
         <p className="text-lg mb-4">نصلك في أقل من 30 دقيقة!</p>
-        <a
-          href="tel:0123456789"
-          className="bg-blue-500 text-white px-6 py-2 rounded-full text-lg font-semibold hover:bg-blue-600 transition duration-300 inline-flex items-center"
-        >
-          <Phone className="h-5 w-5 mr-2 ml-2" />
-          0123456789
-        </a>
+        <div className="flex justify-center space-x-4 space-x-reverse">
+            
+          <a 
+              href="tel:0535142000" 
+              title="اجراء مكالمة هاتفية"
+              className="bg-blue-600 hover:from-indigo-700 hover:bg-blue-800 text-white px-6 py-2 rounded-full flex items-center transition duration-300 hover:scale-105"
+              aria-label="اتصل الآن على الرقم 0535142000"
+            >
+              <FaPhone className="ml-2" />
+              اتصل الآن
+            </a>
+
+            <a 
+              href="https://wa.me/966535142000?text=مرحبا،%20أحتاج%20إلى%20خدمة%20سطحة" 
+              title="الذهاب الى محادثة واتساب"
+              className="bg-green-500 hover:bg-green-700 text-white px-6 py-2 rounded-full flex items-center hover:bg-green-600 transition duration-300 hover:scale-105"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="تواصل عبر واتساب للحصول على خدمة سطحة"
+            >
+              <FaWhatsapp className="ml-2 size-5" />
+              واتساب
+            </a>
+          </div>
       </div>
     </div>
   )
